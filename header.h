10ad58155010a2c95252ffca3bdd43f9bf3cc32f
aneesshah1998 <42958637+aneesshah1998@users.noreply.github.com>
2018-09-25 17:11:57 +0000

    Delete header.h

--- header.h
+++
@@ -1,5 +0,0 @@
-int reverse(int a[],int array_size);
-int rotate(int a [],int array_size);
-int sort(int a[],int array_size);
-int un_sort(int a[],int array_size);
-int s_sort(int a[],int array_size,int x,int p);
